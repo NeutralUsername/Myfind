78f1af333cdd2125d7ba06ff8ae7096ffeeb4420
fabian <fabian@fabian.at>
2023-03-27 11:14:25 +0200

    refactoring

--- myfind.c
+++ myfind.c
@@ -20,6 +20,7 @@ typedef struct Expression {
 
 expressionType getExpressionType(char *expression);
 void printExpression(Expression *expression, int i);
+void processArgs(int argc, char *argv[], Expression **expressions, int *expressionCount, char ***paths, int *pathCount);
 
 int main(int argc, char *argv[])
 {
@@ -28,34 +29,7 @@ int main(int argc, char *argv[])
     int pathCount = 0;
     char **paths = malloc(sizeof(char*));
     paths[0] = "/";
-    for (int i = 1; i < argc; i++)
-    {
-        if (argv[i][0] != '-') {
-            if (expressionCount == 0) {
-                paths = realloc(paths, sizeof(char*) * pathCount+1);
-                paths[pathCount++] = argv[i];
-            } else {
-                printf("find: path must preceed expression: `%s'\n", argv[i]);
-                return 1;
-            }
-            continue;
-        }
-
-        expressionType type = getExpressionType(argv[i]);
-        if (type == invalid) {
-            printf("find: unknown predicate `%s'\n", argv[i]);
-            return 1;
-        }
-        expressions = realloc(expressions, sizeof(Expression) * expressionCount+1);
-        expressions[expressionCount].type = type;
-        if (type == NAME || type == TYPE || type == USER) {
-            expressions[expressionCount].argument = argv[++i];
-        }
-        expressionCount++;
-    } 
-    if (pathCount == 0) {
-        pathCount = 1;
-    }
+    processArgs(argc, argv, &expressions, &expressionCount, &paths, &pathCount);
     for (int i = 0; i < expressionCount; i++) {
         printExpression(&expressions[i], i);
     }
@@ -63,6 +37,37 @@ int main(int argc, char *argv[])
         printf("Path: %s\n", paths[i]);
     }
     return 0;
+}
+
+void processArgs(int argc, char *argv[], Expression **expressions, int *expressionCount, char ***paths, int *pathCount) {
+    for (int i = 1; i < argc; i++)
+    {
+        if (argv[i][0] != '-') {
+            if (*expressionCount == 0) {
+                *paths = realloc(*paths, sizeof(char*) * (*pathCount)+1);
+                (*paths)[(*pathCount)++] = argv[i];
+            } else {
+                printf("find: path must preceed expression: `%s'\n", argv[i]);
+                exit(1);
+            }
+            continue;
+        }
+
+        expressionType type = getExpressionType(argv[i]);
+        if (type == invalid) {
+            printf("find: unknown predicate `%s'\n", argv[i]);
+            exit(1);
+        }
+        *expressions = realloc(*expressions, sizeof(Expression) * (*expressionCount)+1);
+        (*expressions)[*expressionCount].type = type;
+        if (type == NAME || type == TYPE || type == USER) {
+            (*expressions)[*expressionCount].argument = argv[++i];
+        }
+        (*expressionCount)++;
+    } 
+    if (*pathCount == 0) {
+        *pathCount = 1;
+    } 
 }
 
 void printExpression(Expression *expression, int i) {
